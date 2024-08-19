a07d8ea44ef65c3b7535f1e22b666b903ed66bcd
Ashluna22 <ashleyvera032@gmail.com>
2024-08-18 23:06:36 -0400

    first push

---
+++ Basic-for-loop/Basic-for-loop/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+namespace Basic_for_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool continueProgram = true;
+
+            while (continueProgram)
+            {
+                Console.Write("Enter a number: ");
+                int number = int.Parse(Console.ReadLine());
+
+                int sum = 0;
+                for (int i = 1; i <= number; i++)
+                {
+                    sum += i;
+                }
+
+                Console.WriteLine(sum);
+
+                Console.Write("Would you like to continue (y/n)? ");
+                string response = Console.ReadLine().ToLower();
+
+                if (response != "y")
+                {
+                    continueProgram = false;
+                    Console.WriteLine("Goodbye!");
+                }
+            }
+        }
+    }
+}
+    
+
